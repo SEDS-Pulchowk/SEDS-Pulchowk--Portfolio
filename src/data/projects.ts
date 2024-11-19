fbb790189f92880e584842811beba5be59af4505
sandipkatel <babusandipkatel@gmail.com>
2024-11-19 09:17:56 +0545

    changed pic import spell 3

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ const projects = [
     title: "SEDS Earth Information",
     description:
       "The SEDS Earth Information project aims to leverage satellite data to monitor and analyze Earth's environmental changes, contributing to sustainable development and disaster management.",
-    image: "/images/projects/earth_information.png",
+    image: "/images/Projects/earth_information.png",
     year: 2024,
   },
   {
@@ -12,7 +12,7 @@ const projects = [
     title: "Cansat Launching and Testing",
     description:
       "Our SEDS team also have successfully launched cansat as the demonstration of the cansat launching and working process from top of ICTC building at final fay of LOCUS program.",
-    image: "/images/projects/cansat_launching.png",
+    image: "/images/Projects/cansat_launching.png",
     year: 2024,
   },
   {
@@ -20,7 +20,7 @@ const projects = [
     title: "Germination & Comparison of Space-Flown Holy Basil Seeds",
     description:
       "This project aims to study the impact of spaceflight on the germination and growth of holy basil seeds by comparing the performance of space-flown seeds with their earthbound counterparts.",
-    image: "/images/projects/AHiS.png",
+    image: "/images/Projects/AHiS.png",
     year: 2022,
   },
 ];
